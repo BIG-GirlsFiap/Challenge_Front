c563c06a518ceff5ea51ab2d5b3a178151878c3f
Beatriz <rm5544044@fiap.com.br>
2023-11-17 21:03:34 -0300

    Adicionando funções de JavaScript

--- js.js
+++ js.js
@@ -1,34 +1,87 @@
 const campoUser = document.querySelector("#user");
 const campoSenha = document.querySelector("#senha");
-const buttonEnviar = document.querySelector("button");
+const buttonEnviar = document.querySelector(".buttonForm1");
+const campoData = document.querySelector("#data");
+const campoExame = document.querySelector("#Exames");
+const buttonEnviar2 = document.querySelector(".sendButton");
 
-// Criando um elemento p
+const buttonExpand = document.querySelector(".BTipos");
+const exames = document.querySelector(".Tipos")
+
+
 const pMensagem = document.createElement("p");
-
+pMensagem.setAttribute("class", "erro");
 
 function clearForm() {
     campoUser.value = '';
     campoSenha.value = '';
+    campoData.value = '';
+    campoExame.value = '';
 }
 
+function mostrarMensagemErro(campo, mensagem) {
+    pMensagem.textContent = mensagem;
+    campo.parentNode.insertBefore(pMensagem, campo.nextSibling);
+}
 
-buttonEnviar.addEventListener("click", function(event){
+function esconderMensagemErro() {
+    pMensagem.textContent = '';
+}
+
+buttonEnviar.addEventListener("click", function (event) {
     event.preventDefault();
-    if (campoUser.value === ''){
-        // alert("Preencha o campo nome!");
-        pMensagem.textContent = "Preencha o campo Email!";
-        campoUser.insertAdjacentElement("afterend", pMensagem);
-        pMensagem.setAttribute("class", "erro");
+    if (campoUser.value === '') {
+        mostrarMensagemErro(campoUser, "Preencha o campo Email!");
         return false;
     }
 
-    if (campoSenha.value === ''){
-        // alert("Preencha o campo cidade!");
-        pMensagem.textContent = "Preencha o campo Senha!";
-        campoSenha.insertAdjacentElement("afterend", pMensagem);
-        pMensagem.setAttribute("class", "erro");   
+    if (campoSenha.value === '') {
+        mostrarMensagemErro(campoSenha, "Preencha o campo Senha!");
         return false;
     }
-    pMensagem.classList.add('d-none');
-    clearForm();    
+
+    esconderMensagemErro();
+    clearForm();
 });
+
+buttonEnviar2.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (campoData.value === '') {
+        mostrarMensagemErro(campoData, "Preencha o campo Data!");
+        return false;
+    }
+    if (campoExame.value === '') {
+        mostrarMensagemErro(campoExame, "Preencha o campo Exame")
+        return false;
+    }
+
+    esconderMensagemErro();
+    clearForm();
+
+});
+
+buttonExpand.addEventListener("click", function(){
+    exames.classList.add("d-block");
+});
+
+const tristeza = document.querySelector(".botao-com-imagem1");
+const raiva = document.querySelector(".botao-com-imagem2");
+const alegria = document.querySelector(".botao-com-imagem3");
+const medo = document.querySelector(".botao-com-imagem4");
+
+tristeza.addEventListener("click", function(){
+    alert("Obrigado por avaliar!");
+})
+
+raiva.addEventListener("click", function(){
+    alert("Obrigado por avaliar!");
+})
+
+alegria.addEventListener("click", function(){
+    alert("Obrigado por avaliar!");
+})
+
+medo.addEventListener("click", function(){
+    alert("Obrigado por avaliar!");
+})
+
